d7c11b2c487a9e251f4624d26ae1384bc9a03ad5
Артём Ильин <artemilin347@gmail.com>
2024-04-20 11:02:53 +0300

    Удалить шаблон backend'а

--- backend/my_app_api/exceptions.py
+++
@@ -1,5 +0,0 @@
-class APIError(Exception):
-    """Base class for API errors"""
-
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
--- backend/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from my_app_api.routes import app
-
-
-@pytest.fixture
-def client():
-    client = TestClient(app)
-    yield client
